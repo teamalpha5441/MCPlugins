f9ec98a212678c54e97c00d239e15ed6d57545c7
teamalpha5441 <teamalpha5441@gmail.com>
2015-10-23 23:03:27 +0200

    Added TAKeepLevels

---
+++ TAKeepLevels/src/main/java/me/teamalpha5441/mcplugins/takeeplevels/TAKeepLevels.java
@@ -0,0 +1,20 @@
+package me.teamalpha5441.mcplugins.takeeplevels;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TAKeepLevels extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void onPlayerDeathEvent(PlayerDeathEvent evt) {
+		evt.setKeepLevel(true);
+		evt.setDroppedExp(0);
+	}
+}
