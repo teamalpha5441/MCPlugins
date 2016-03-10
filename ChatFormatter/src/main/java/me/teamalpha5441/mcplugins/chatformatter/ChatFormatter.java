0dc52fc3589191f626b08bfeebdf656eb6a1b620
teamalpha5441 <teamalpha5441@gmail.com>
2016-03-10 21:20:59 +0100

    Added ChatFormatter

---
+++ ChatFormatter/src/main/java/me/teamalpha5441/mcplugins/chatformatter/ChatFormatter.java
@@ -0,0 +1,40 @@
+package me.teamalpha5441.mcplugins.chatformatter;
+
+import java.util.logging.Level;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatFormatter extends JavaPlugin implements Listener {
+
+	private volatile String formatString;
+	
+	@Override
+	public void onLoad() {
+		saveDefaultConfig();
+	}
+	
+	@Override
+	public void onEnable() {
+		this.formatString = getConfig().getString("format-string");
+		if (this.formatString != null) {
+			this.formatString = ChatColor.translateAlternateColorCodes('&', this.formatString);
+			getServer().getPluginManager().registerEvents(this, this);
+		} else {
+			getLogger().log(Level.WARNING, "No format string defined in config, using default format");
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerChatEvent(AsyncPlayerChatEvent evt) {
+		if (this.formatString != null) {
+			evt.setFormat(this.formatString);
+		}
+		if (evt.getPlayer().hasPermission("chatformatter.colorcodes")) {
+			evt.setMessage(ChatColor.translateAlternateColorCodes('&', evt.getMessage()));
+		}
+	}
+}
