44189fc642661714d6e27c6a217424d0e1bf6d00
teamalpha5441 <teamalpha5441@gmail.com>
2016-03-29 00:55:08 +0200

    Added RealTime

---
+++ RealTime/src/main/java/me/teamalpha5441/mcplugins/realtime/RealTime.java
@@ -0,0 +1,58 @@
+package me.teamalpha5441.mcplugins.realtime;
+
+import java.util.Date;
+import java.util.List;
+
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RealTime extends JavaPlugin implements Runnable {
+
+	private int taskID;
+	private World[] worlds;
+	
+	@Override
+	public void onLoad() {
+		saveDefaultConfig();
+	}
+	
+	@Override
+	public void onEnable() {
+		this.taskID = getServer().getScheduler().scheduleSyncRepeatingTask(this, this, 0, 72);
+		if (this.taskID < 0) {
+			throw new RuntimeException("Couldn't start task");
+		}
+		
+		List<String> strWorlds = getConfig().getStringList("worlds");
+		if (strWorlds != null) {
+			this.worlds = new World[strWorlds.size()];
+			for (int i = 0; i < worlds.length; i++) {
+				this.worlds[i] = getServer().getWorld(strWorlds.get(i));
+				this.worlds[i].setGameRuleValue("doDaylightCycle", "false");
+			}
+		} else {
+			throw new RuntimeException("Couldn't retrieve worlds from config");
+		}
+	}
+	
+	@Override
+	public void onDisable() {
+		getServer().getScheduler().cancelTask(this.taskID);
+	}
+
+	@Override
+	public void run() {
+		Date currentTime = new Date();
+		float realHours = currentTime.getHours();
+		realHours += currentTime.getMinutes() / 60f;
+		realHours += currentTime.getSeconds() / 3600f;
+		int mcTime = (int)(realHours * 1000 + 0.5f) - 6000;
+		if (mcTime < 0) {
+			mcTime += 24000;
+		}
+		
+		for (World world : worlds) {
+			world.setFullTime(mcTime);
+		}
+	}
+}
